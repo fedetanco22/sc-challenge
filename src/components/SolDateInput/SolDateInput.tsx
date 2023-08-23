fb48c00ed6cad33571499fa7dda669b45ce6c6e0
Federico Tanco <fede@stagefi.com>
2023-08-22 23:51:30 -0300

    solDateInput null

---
+++ src/components/SolDateInput/SolDateInput.tsx
@@ -0,0 +1,20 @@
+function SolDateInput(): null {
+  return null
+  // const handleSolDateChange = event => {
+  //   handleSolDate(event.target.value)
+  // }
+
+  // return (
+  //   <>
+  //     <input
+  //       type="number"
+  //       onChange={handleSolDateChange}
+  //       value={sol || ''}
+  //       placeholder={`Enter a sol date between 0 and ${maxSol}`}
+  //     />
+  //   </>
+  // )
+}
+
+export default SolDateInput
+// { maxSol, handleSolDate, sol } PROPS
